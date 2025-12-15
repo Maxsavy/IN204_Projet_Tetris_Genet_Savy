0c0ea5fa32c51ff0f58e4102303e5cc461d2596b
Maxsavy <maxime.savy@polymtl.ca>
2025-12-15 11:48:36 +0100

    first functionning menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include "ui/menu.cpp"
 #include <iostream>
 #include <vector>
 
@@ -6,59 +7,8 @@ using namespace std;
 
 int main()
 {
-    // --- LOADING FONT AND IMAGE ---
-    sf::Font font;
-
-    if (!font.loadFromFile("assets/fonts/arcade.ttf"))
-    {
-        cout << "pas de font lol" << endl;
-        return -1;
-    }
-
-    sf::Texture background;
-    if (!background.loadFromFile("assets/images/background_menu.jpg"))
-    {
-        cout << "pas d'image lol" << endl;
-        return -1;
-    }
-
-    // --- SETTING THE WINDOW ---
-    unsigned int windowWidth = 1280;
-    unsigned int windowHeight = 720;
-
     sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Tetris");
-    window.setFramerateLimit(144);
-
-    // --- SETUP SPRITE ---
-    sf::Sprite back_sprite(background);
-
-    sf::Vector2u imageSize = background.getSize();
-    float scaleX = static_cast<float>(windowWidth) / imageSize.x;
-    float scaleY = static_cast<float>(windowHeight) / imageSize.y;
-
-    back_sprite.setScale(scaleX, scaleY);
-
-    // --- SETTING TITLE ---
-    sf::Text title("TETRIS SA MERE", font, 90);
-    title.setFillColor(sf::Color::White);
-    title.setStyle(sf::Text::Bold);
-    title.setPosition(550.f, 100.f);
-
-    // --- GAME LOOP ---
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear(sf::Color::Black);
-        window.draw(back_sprite);
-        window.draw(title);
-        window.display();
-    }
-
+    game::MainMenu menu(window);
+    menu.start();
     return 0;
 }
---
+++ src/ui/menu.cpp
@@ -0,0 +1,116 @@
+#include "menu.h"
+#include <iostream>
+using namespace std;
+
+unsigned int windowWidth = 1280;
+unsigned int windowHeight = 720;
+
+namespace game
+{
+    sf::Texture background;
+    sf::Sprite background_sprite;
+
+    MainMenu::MainMenu(sf::RenderWindow &w)
+        : _window(w), _main_menu_context(nullptr, menu_destroy_context)
+    {
+        setup_menu_context();
+    }
+
+    void MainMenu::start()
+    {
+        _window.setFramerateLimit(144);
+        while (_window.isOpen())
+        {
+            if (_is_exit_requested)
+            {
+                _window.close();
+                break;
+            }
+            sf::Event event;
+            while (_window.pollEvent(event))
+            {
+                if (event.type == sf::Event::Closed)
+                {
+                    _is_exit_requested = true;
+                    break;
+                }
+                menu_handle_event(_current_menu, event);
+            }
+            _window.clear();
+            // draw background if available
+            if (background.getSize().x > 0 && background.getSize().y > 0)
+            {
+                _window.draw(background_sprite);
+            }
+            menu_render(_current_menu);
+            _window.display();
+        }
+    }
+
+    void MainMenu::setup_menu_context()
+    {
+        if (!_font.loadFromFile("assets/fonts/arcade.ttf"))
+        {
+            cerr << "Failed to load font: arcade.ttf (tried multiple locations)" << endl;
+        }
+
+        if (!background.loadFromFile("assets/images/background_menu.jpg"))
+        {
+            cerr << "Failed to load background image: assets/images/background_menu.jpg" << endl;
+        }
+        else
+        {
+            background_sprite.setTexture(background);
+            sf::Vector2u imageSize = background.getSize();
+            if (imageSize.x > 0 && imageSize.y > 0)
+            {
+                float scaleX = static_cast<float>(_window.getSize().x) / imageSize.x;
+                float scaleY = static_cast<float>(_window.getSize().y) / imageSize.y;
+                background_sprite.setScale(scaleX, scaleY);
+                background_sprite.setPosition(0.f, 0.f);
+            }
+        }
+        game_menu::Style style{.TitleFont = &_font,
+                               .ItemFont = &_font,
+                               .TitleFontSize = 50,
+                               .ItemFontSize = 35,
+                               .MenuTitleScaleFactor = 1,
+                               .MenuItemScaleFactor = 1.5,
+                               .colorScheme = {.titleColor = 0xFFFFFF,
+                                               .itemColor = 0xFFFFFF,
+                                               .selectedColor = 0xFF22F1},
+                               .PaddingTitle =
+                                   {
+                                       .top = 100,
+                                       .left = 0,
+                                   },
+                               .PaddingItems =
+                                   {
+                                       .top = 30,
+                                   },
+                               .TitleAlign = game_menu::Align::Center,
+                               .ItemAlign = game_menu::Align::Center};
+
+        std::vector<game_menu::MenuItem> items{
+            {"New Game",
+             [&](sf::RenderTarget &target)
+             {
+                 cout << "lancer jeu!" << endl;
+             }},
+            {"Leaderboard", [](sf::RenderTarget &target)
+             {
+                 cout << "leaderboard!" << endl;
+             }},
+            {"Settings", [](sf::RenderTarget &target)
+             {
+                 cout << "settings!" << endl;
+             }},
+            {"Exit", [&](sf::RenderTarget &target)
+             { this->_is_exit_requested = true; }}};
+
+        game_menu::MenuConfig config{
+            .title = " Tetris sa mere", .items = items, .style = style};
+        _main_menu_context.reset(create_menu_context(_window, config));
+        _current_menu = _main_menu_context.get();
+    }
+}
